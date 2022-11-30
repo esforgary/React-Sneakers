d4550f48fa9b82e1cdf72f3854b3f15ee00cef67
Staniclav Najdenov <73990417+esforgary@users.noreply.github.com>
2022-11-30 22:31:41 +0200

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,9 +11,9 @@ function Header() {
   const {totalPrice} = useCart();
     return (
         <header className="d-flex justify-between align-center p-40">
-          <Link to="">
+          <Link to={process.env.PUBLIC_URL + "/"}>
             <div className="d-flex align-center">
-               <img height={40} width={40} src="img/logo.png" alt="logo"/>
+               <img height={40} width={40} src={process.env.PUBLIC_URL + "/img/logo.png"} alt="logo"/>
                 <div>
                     <h3 className="text-uppercase">React Sneakers</h3>
                     <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -22,17 +22,17 @@ function Header() {
           </Link>
             <ul className="d-flex">
                 <li onClick={() => setCartOpened(true)} className="mr-25 cu-p">
-                    <img height={18} width={18} src="img/cart.svg" alt="cart"/>
+                    <img height={18} width={18} src={process.env.PUBLIC_URL + "/img/cart.svg"} alt="cart"/>
                     <span>{totalPrice > 0 ? `${totalPrice} грн.` : null}</span>
                 </li>
                 <li className="mr-20 cu-p">
-                    <Link to="favorites">
-                      <img height={18} width={18} src="img/favorite.svg" alt="favorite"/>
+                    <Link to={process.env.PUBLIC_URL + "/favorites"}>
+                      <img height={18} width={18} src={ process.env.PUBLIC_URL + "/img/favorite.svg"} alt="favorite"/>
                     </Link>
                 </li>
                 <li className="cu-p">
-                  <Link to="orders">
-                    <img height={18} width={18} src="img/user.svg" alt="user"/>
+                  <Link to={process.env.PUBLIC_URL + "/orders"}>
+                    <img height={18} width={18} src={process.env.PUBLIC_URL + "/img/user.svg"} alt="user"/>
                     </Link>
                 </li>
             </ul>            
